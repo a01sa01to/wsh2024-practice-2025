ee2591160893e84c414ae6b1729ea2d034a9b1f2
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-01 18:35:43 +0900

    format

--- workspaces/app/src/lib/filter/isContains.ts
+++ workspaces/app/src/lib/filter/isContains.ts
@@ -5,10 +5,10 @@ type Params = {
 
 // ひらがな・カタカナ・半角・全角を区別せずに文字列が含まれているかを調べる
 export function isContains({ query, target }: Params): boolean {
-  const collator = new Intl.Collator("ja", { sensitivity: "accent", usage: "search" });
+  const collator = new Intl.Collator('ja', { sensitivity: 'accent', usage: 'search' });
   for (let offset = 0; offset <= target.length - query.length; offset++) {
     const targetPart = target.slice(offset, offset + query.length);
-    if (collator.compare(targetPart, query) !== 0) continue
+    if (collator.compare(targetPart, query) !== 0) continue;
     return true;
   }
   return false;
