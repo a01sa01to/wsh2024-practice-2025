db122bd97d42096fa5713a033c40f711231f79d5
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-05 19:27:53 +0900

    fix: separator の canvas やめて base64 化

--- workspaces/app/src/foundation/components/Separator.tsx
+++ workspaces/app/src/foundation/components/Separator.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-
-import { Color } from '../styles/variables';
 
 const _Wrapper = styled.div`
   width: 100%;
@@ -14,36 +11,11 @@ const _Separator = styled.img`
 `;
 
 export const Separator: React.FC = () => {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  const [imgUrl, setImgUrl] = useState<string | null>(null);
-
-  useEffect(() => {
-    const width = wrapperRef.current?.clientWidth;
-
-    const canvas = document.createElement('canvas');
-    canvas.width = width ?? 0;
-    canvas.height = 1;
-
-    const ctx = canvas.getContext('2d');
-
-    if (ctx == null) {
-      return;
-    }
-
-    ctx.moveTo(0, 0);
-    ctx.lineTo(width ?? 0, 0);
-
-    ctx.strokeStyle = Color.MONO_30;
-    ctx.lineWidth = 1;
-
-    ctx.stroke();
-
-    setImgUrl(canvas.toDataURL('image/png'));
-  }, []);
-
+  const imgUrl =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjuHz5cj0AB+8C+UyZ3vUAAAAASUVORK5CYII=';
   return (
-    <_Wrapper ref={wrapperRef}>
-      {imgUrl != null ? <_Separator aria-hidden={true} height={1} src={imgUrl} width="100%" /> : null}
+    <_Wrapper>
+      <_Separator aria-hidden={true} height={1} src={imgUrl} width="100%" />
     </_Wrapper>
   );
 };
