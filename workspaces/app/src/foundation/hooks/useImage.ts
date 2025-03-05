4b3f0d168153e1da43b3f7a05ebc223db26d28fc
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-05 17:12:04 +0900

    fix: dpr のせいで not found 言われてますが

--- workspaces/app/src/foundation/hooks/useImage.ts
+++ workspaces/app/src/foundation/hooks/useImage.ts
@@ -1,7 +1,8 @@
 import { getImageUrl } from '../../lib/image/getImageUrl';
 
 export const useImage = ({ height, imageId, width }: { height: number; imageId: string; width: number }) => {
-  const dpr = window.devicePixelRatio;
+  // const dpr = window.devicePixelRatio;
+  const dpr = 1;
   return getImageUrl({
     format: 'avif',
     height: height * dpr,
