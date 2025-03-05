565ade1c679e3b93a1c2264f3b294b71c30ebf33
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-06 00:13:54 +0900

    format

--- workspaces/admin/src/lib/image/isSupportedImage.ts
+++ workspaces/admin/src/lib/image/isSupportedImage.ts
@@ -1,4 +1,3 @@
-
 const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif', 'image/jxl'];
 
 export async function isSupportedImage(image: File): Promise<boolean> {
--- workspaces/client/src/index.admin.tsx
+++ workspaces/client/src/index.admin.tsx
@@ -10,10 +10,7 @@ const main = async () => {
   const fn = () => {
     const root = document.getElementById('root');
     if (!root) throw new Error('Root element not found');
-    ReactDOM.hydrateRoot(
-      root,
-      <AdminApp />
-    );
+    ReactDOM.hydrateRoot(root, <AdminApp />);
   };
 
   if (document.readyState === 'loading') {
