2596ba8aea61455b52f267a72b355c2bad1348fc
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-06 13:13:39 +0900

    scroll snap 計算を極力減らす

--- workspaces/app/src/features/viewer/components/ComicViewerCore.tsx
+++ workspaces/app/src/features/viewer/components/ComicViewerCore.tsx
@@ -1,26 +1,12 @@
 import { Suspense, useEffect, useState } from 'react';
-import { useInterval, useUpdate } from 'react-use';
 import styled from 'styled-components';
 
 import { useEpisode } from '../../episode/hooks/useEpisode';
 
 import { ComicViewerPage } from './ComicViewerPage';
 
-// const IMAGE_WIDTH = 1075;
-// const IMAGE_HEIGHT = 1518;
-const IMAGE_WIDTH = 600;
-const IMAGE_HEIGHT = 850;
-
 /** スクロールスナップで適切な位置になるための X 軸の移動距離を計算する */
-function getScrollToLeft({
-  pageCountParView,
-  pageWidth,
-  scrollView,
-}: {
-  pageCountParView: number;
-  pageWidth: number;
-  scrollView: HTMLDivElement;
-}) {
+function getScrollToLeft({ scrollView }: { scrollView: HTMLDivElement }) {
   const scrollViewClientRect = scrollView.getBoundingClientRect();
   const scrollViewCenterX = (scrollViewClientRect.left + scrollViewClientRect.right) / 2;
 
@@ -29,38 +15,41 @@ function getScrollToLeft({
   let scrollToLeft = Number.MAX_SAFE_INTEGER;
 
   // 画面に表示されているページの中心と、スクロールビューの中心との差分を計算する
-  for (let times = 0; times < 10; times++) {
-    for (const [idx, child] of children.entries()) {
-      const nthChild = idx + 1;
-      const elementClientRect = child.getBoundingClientRect();
+  for (const [idx, child] of children.entries()) {
+    const nthChild = idx + 1;
+    const elementClientRect = child.getBoundingClientRect();
 
-      // 見開き2ページの場合は、scroll-margin で表示領域にサイズを合わせる
-      const scrollMargin =
-        pageCountParView === 2
-          ? {
-            // 奇数ページのときは左側に1ページ分の幅を追加する
-            left: nthChild % 2 === 0 ? pageWidth : 0,
-            // 偶数ページのときは右側に1ページ分の幅を追加する
-            right: nthChild % 2 === 1 ? pageWidth : 0,
-          }
-          : { left: 0, right: 0 };
+    // CSS 変数から取得
+    const pageCountParView = Number(getComputedStyle(scrollView).getPropertyValue('--pagecnt'));
 
-      // scroll-margin の分だけ広げた範囲を計算する
-      const areaClientRect = {
-        bottom: elementClientRect.bottom,
-        left: elementClientRect.left - scrollMargin.left,
-        right: elementClientRect.right + scrollMargin.right,
-        top: elementClientRect.top,
-      };
+    const pageWidth = elementClientRect.width;
 
-      const areaCenterX = (areaClientRect.left + areaClientRect.right) / 2;
-      // ページの中心をスクロールビューの中心に合わせるための移動距離
-      const candidateScrollToLeft = areaCenterX - scrollViewCenterX;
+    // 見開き2ページの場合は、scroll-margin で表示領域にサイズを合わせる
+    const scrollMargin =
+      pageCountParView === 2
+        ? {
+          // 奇数ページのときは左側に1ページ分の幅を追加する
+          left: nthChild % 2 === 0 ? pageWidth : 0,
+          // 偶数ページのときは右側に1ページ分の幅を追加する
+          right: nthChild % 2 === 1 ? pageWidth : 0,
+        }
+        : { left: 0, right: 0 };
 
-      // もっともスクロール量の少ないものを選ぶ
-      if (Math.abs(candidateScrollToLeft) < Math.abs(scrollToLeft)) {
-        scrollToLeft = candidateScrollToLeft;
-      }
+    // scroll-margin の分だけ広げた範囲を計算する
+    const areaClientRect = {
+      bottom: elementClientRect.bottom,
+      left: elementClientRect.left - scrollMargin.left,
+      right: elementClientRect.right + scrollMargin.right,
+      top: elementClientRect.top,
+    };
+
+    const areaCenterX = (areaClientRect.left + areaClientRect.right) / 2;
+    // ページの中心をスクロールビューの中心に合わせるための移動距離
+    const candidateScrollToLeft = areaCenterX - scrollViewCenterX;
+
+    // もっともスクロール量の少ないものを選ぶ
+    if (Math.abs(candidateScrollToLeft) < Math.abs(scrollToLeft)) {
+      scrollToLeft = candidateScrollToLeft;
     }
   }
 
@@ -83,8 +72,23 @@ const _Wrapper = styled.div`
   overflow-x: scroll;
   overflow-y: hidden;
   overscroll-behavior: none;
-  padding-inline: calc((min(100vw, 1024px) - var(--manga-page-width) * var(--pagecnt)) / 2 + var(--manga-page-width) * (var(--pagecnt) - 1));
+  padding-inline: calc(
+    (min(100vw, 1024px) - var(--manga-page-width) * var(--pagecnt)) / 2 + var(--manga-page-width) * (var(--pagecnt) - 1)
+  );
   touch-action: none;
+
+  scroll-behavior: smooth;
+  scroll-snap-type: x mandatory;
+  > * {
+    scroll-snap-align: center;
+
+    &:nth-child(even) {
+      scroll-margin-left: calc((var(--pagecnt) - 1) * var(--manga-page-width));
+    }
+    &:nth-child(odd) {
+      scroll-margin-right: calc((var(--pagecnt) - 1) * var(--manga-page-width));
+    }
+  }
 
   &::-webkit-scrollbar {
     display: none;
@@ -96,37 +100,21 @@ type Props = {
 };
 
 const ComicViewerCore: React.FC<Props> = ({ episodeId }) => {
-  // 画面のリサイズに合わせて再描画する
-  const rerender = useUpdate();
-  useInterval(rerender, 0);
-
   const { data: episode } = useEpisode({ params: { episodeId } });
 
-  const [container, containerRef] = useState<HTMLDivElement | null>(null);
   const [scrollView, scrollViewRef] = useState<HTMLDivElement | null>(null);
-
-  // コンテナの幅
-  const cqw = (container?.getBoundingClientRect().width ?? 0) / 100;
-  // コンテナの高さ
-  const cqh = (container?.getBoundingClientRect().height ?? 0) / 100;
-
-  // 1画面に表示できるページ数（1 or 2）
-  const pageCountParView = (100 * cqw) / (100 * cqh) < (2 * IMAGE_WIDTH) / IMAGE_HEIGHT ? 1 : 2;
-  // ページの幅
-  const pageWidth = ((100 * cqh) / IMAGE_HEIGHT) * IMAGE_WIDTH;
 
   useEffect(() => {
     const abortController = new AbortController();
 
     let isPressed = false;
-    let scrollToLeftWhenScrollEnd = 0;
 
     const handlePointerDown = (ev: PointerEvent) => {
       const scrollView = ev.currentTarget as HTMLDivElement;
       isPressed = true;
       scrollView.style.cursor = 'grabbing';
       scrollView.setPointerCapture(ev.pointerId);
-      scrollToLeftWhenScrollEnd = getScrollToLeft({ pageCountParView, pageWidth, scrollView });
+      scrollView.style.scrollSnapType = 'none';
     };
 
     const handlePointerMove = (ev: PointerEvent) => {
@@ -136,71 +124,44 @@ const ComicViewerCore: React.FC<Props> = ({ episodeId }) => {
           behavior: 'instant',
           left: -1 * ev.movementX,
         });
-        scrollToLeftWhenScrollEnd = getScrollToLeft({ pageCountParView, pageWidth, scrollView });
       }
     };
 
     const handlePointerUp = (ev: PointerEvent) => {
       const scrollView = ev.currentTarget as HTMLDivElement;
       isPressed = false;
-      scrollView.style.cursor = 'grab';
       scrollView.releasePointerCapture(ev.pointerId);
-      scrollToLeftWhenScrollEnd = getScrollToLeft({ pageCountParView, pageWidth, scrollView });
-    };
-
-    const handleScroll = (ev: Pick<Event, 'currentTarget'>) => {
-      const scrollView = ev.currentTarget as HTMLDivElement;
-      scrollToLeftWhenScrollEnd = getScrollToLeft({ pageCountParView, pageWidth, scrollView });
-    };
-
-    let scrollEndTimer = -1;
-    abortController.signal.addEventListener('abort', () => window.clearTimeout(scrollEndTimer), { once: true });
-
-    const handleScrollEnd = (ev: Pick<Event, 'currentTarget'>) => {
-      const scrollView = ev.currentTarget as HTMLDivElement;
-
-      // マウスが離されるまではスクロール中とみなす
-      if (isPressed) {
-        scrollEndTimer = window.setTimeout(() => handleScrollEnd({ currentTarget: scrollView }), 0);
-        return;
-      } else {
+      scrollView.style.cursor = 'grab';
+      const diff = getScrollToLeft({ scrollView });
+      if (diff === 0) {
+        // scrollend event が発火しないので
+        scrollView.style.scrollSnapType = 'x mandatory';
+      }
+      else {
         scrollView.scrollBy({
           behavior: 'smooth',
-          left: scrollToLeftWhenScrollEnd,
+          left: diff,
         });
       }
     };
 
-    let prevContentRect: DOMRectReadOnly | null = null;
-    const handleResize = (entries: ResizeObserverEntry[]) => {
-      if (prevContentRect != null && prevContentRect.width !== entries[0]?.contentRect.width) {
-        requestAnimationFrame(() => {
-          scrollView?.scrollBy({
-            behavior: 'instant',
-            left: getScrollToLeft({ pageCountParView, pageWidth, scrollView }),
-          });
-        });
-      }
-      prevContentRect = entries[0]?.contentRect ?? null;
+    const handleScrollEnd = (ev: Event) => {
+      const scrollView = ev.currentTarget as HTMLDivElement;
+      if (!isPressed) scrollView.style.scrollSnapType = 'x mandatory';
     };
 
     scrollView?.addEventListener('pointerdown', handlePointerDown, { passive: false, signal: abortController.signal });
     scrollView?.addEventListener('pointermove', handlePointerMove, { passive: false, signal: abortController.signal });
     scrollView?.addEventListener('pointerup', handlePointerUp, { passive: false, signal: abortController.signal });
-    scrollView?.addEventListener('scroll', handleScroll, { passive: false, signal: abortController.signal });
     scrollView?.addEventListener('scrollend', handleScrollEnd, { passive: false, signal: abortController.signal });
-
-    const resizeObserver = new ResizeObserver(handleResize);
-    scrollView && resizeObserver.observe(scrollView);
-    abortController.signal.addEventListener('abort', () => resizeObserver.disconnect(), { once: true });
 
     return () => {
       abortController.abort();
     };
-  }, [pageCountParView, pageWidth, scrollView]);
+  }, [scrollView]);
 
   return (
-    <_Container ref={containerRef}>
+    <_Container>
       <_Wrapper ref={scrollViewRef}>
         {episode.pages.map((page) => {
           return <ComicViewerPage key={page.id} pageImageId={page.image.id} />;
