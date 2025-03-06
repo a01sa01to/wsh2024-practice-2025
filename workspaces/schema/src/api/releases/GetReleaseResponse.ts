28ef3b84082ed7efd584de2147a57b1cf52446e4
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-06 17:32:04 +0900

    format

--- workspaces/schema/src/api/releases/GetReleaseResponse.ts
+++ workspaces/schema/src/api/releases/GetReleaseResponse.ts
@@ -3,28 +3,30 @@ import type { z } from 'zod';
 
 import { author, book, image, release } from '../../models';
 
-export const GetReleaseResponseSchema = createSelectSchema(release).pick({}).extend({
-  books: createSelectSchema(book)
-    .pick({
-      id: true,
-      name: true,
-    })
-    .extend({
-      author: createSelectSchema(author)
-        .pick({
-          name: true,
-        })
-        .extend({
-          image: createSelectSchema(image).pick({
-            id: true,
-          }),
-        }),
-      image: createSelectSchema(image).pick({
-        alt: true,
+export const GetReleaseResponseSchema = createSelectSchema(release)
+  .pick({})
+  .extend({
+    books: createSelectSchema(book)
+      .pick({
         id: true,
-      }),
-    })
-    .array(),
-});
+        name: true,
+      })
+      .extend({
+        author: createSelectSchema(author)
+          .pick({
+            name: true,
+          })
+          .extend({
+            image: createSelectSchema(image).pick({
+              id: true,
+            }),
+          }),
+        image: createSelectSchema(image).pick({
+          alt: true,
+          id: true,
+        }),
+      })
+      .array(),
+  });
 
 export type GetReleaseResponse = z.infer<typeof GetReleaseResponseSchema>;
